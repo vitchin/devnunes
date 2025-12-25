bc6f984080b1601e2c48f4cd3f1c0291a792e4e4
Victor Ricardo Oliveira Nunes <victoroliv2004@gmail.com>
2025-12-25 01:33:35 -0300

    [feat] page.tsx | faz com que a home esteja uma camada abaixo da navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <header id="home">
+      <header id="home" className="z-0">
         <Hero />
       </header>
 
       <main className="flex-grow">
-        <section id="about" className="py-20">
+        <section id="about" className="py-20 shadow-2xl">
           <About />
         </section>
 
-        <section id="projects" className="py-20 bg-gray-100">
+        <section id="projects" className="py-20 bg-gray-100 shadow-lg">
           <Projects />
         </section>
 
@@ -28,8 +28,8 @@ export default function Home() {
         </section>
       </main>
 
-      <footer className="text-center p-4 bg-gray-100">
-        <p className="text-sm text-gray-600">© 2024 Your Name. All Rights Reserved.</p>
+      <footer className="text-center p-4 bg-[#0b1423]">
+        <p className="text-sm text-gray-300">© 2025 Your DevNunes. Todos os direitos reservados.</p>
       </footer>
     </div>
   );
